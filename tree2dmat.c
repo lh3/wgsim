c36d512d951e77509baaf134acd605bbeaf61624
Andrew Tritt <ajtritt@lbl.gov>
2019-10-01 13:38:57 -0700

    add options and saving of names

--- tree2dmat.c
+++ tree2dmat.c
@@ -15,41 +15,41 @@ static int usage()
 {
 	fprintf(stderr, "\n");
 	fprintf(stderr, "Usage: tree2dmat  [options] <in.nwk>\n\n");
-    /*
-	fprintf(stderr, "Options: -e FLOAT      base error rate [%.3f]\n", ERR_RATE);
-	fprintf(stderr, "         -d INT        outer distance between the two ends [270]\n");
-	fprintf(stderr, "         -s INT        standard deviation [50]\n");
-	fprintf(stderr, "         -N INT        number of read pairs [1000000]\n");
-	fprintf(stderr, "         -1 INT        length of the first read [150]\n");
-	fprintf(stderr, "         -2 INT        length of the second read [150]\n");
-	fprintf(stderr, "         -r FLOAT      rate of mutations [%.4f]\n", MUT_RATE);
-	fprintf(stderr, "         -R FLOAT      fraction of indels [%.2f]\n", INDEL_FRAC);
-	fprintf(stderr, "         -X FLOAT      probability an indel is extended [%.2f]\n", INDEL_EXTEND);
-	fprintf(stderr, "         -S INT        seed for random generator [-1]\n");
-	fprintf(stderr, "         -A FLOAT      discard if the fraction of ambiguous bases higher than FLOAT [%.2f]\n", MAX_N_RATIO);
-	fprintf(stderr, "         -h            haplotype mode\n");
-	fprintf(stderr, "         -f STR        the directory containing input FastA files [.]\n");
-	fprintf(stderr, "         -o STR        the directory to write files output files to [.]\n");
-	fprintf(stderr, "         -I            interlaved output\n");
+	fprintf(stderr, "Options: -o STR        the HDF5 file to write the distance matrix to [distmat.h5]\n");
+	fprintf(stderr, "         -d STR        the dataset to write the distance matrix to [distances]\n");
+	fprintf(stderr, "         -n STR        the dataset to write the leaf names to [leaf_names]\n");
+	fprintf(stderr, "         -v            verbose output\n");
 	fprintf(stderr, "\n");
-    */
 	return 1;
 }
 
 int main(int argc, char *argv[])
 {
-    char *nwk_path = argv[1];
-    if (argc < 2){
-        return usage();
-    }
-
-    FILE *file = fopen(nwk_path, "r");
-    int n_leaves, i, c;
-    char *code;
+    FILE *file;
+    int n_leaves, i, c, verbose = 0;
+    char *code, *nwk_path,
+         *h5_outpath = "distmat.h5",
+         *h5_dset_path = "distances",
+         *h5_names = "leaf_names";
     size_t n = 0;
 
+	while ((c = getopt(argc, argv, "o:d:n:v")) >= 0) {
+		switch (c) {
+		case 'o': h5_outpath = optarg; break;
+		case 'd': h5_dset_path = optarg; break;
+		case 'n': h5_names = optarg; break;
+		case 'v': verbose = 1; break;
+        }
+    }
+    if (argc - optind < 1) return usage();
 
-    if (file == NULL) return 1; //could not open file
+    nwk_path = argv[optind];
+    if (verbose) printf("reading Newick string from %s\n", nwk_path);
+    file = fopen(nwk_path, "r");
+
+    if (file == NULL) {
+        return 1; //could not open file
+    }
     // compute file size
     fseek(file, 0, SEEK_END);
     long f_size = ftell(file);
@@ -69,6 +69,7 @@ int main(int argc, char *argv[])
     code[n] = '\0';
 
     n_leaves = internodes_n+1;
+    if (verbose) printf("found %d leaves\n", n_leaves);
 
     double * curr_dist = (double *) malloc(sizeof(double)*(n_leaves));
     double * weights = (double *) malloc(sizeof(double)*((n_leaves*(n_leaves-1))/2));
@@ -83,38 +84,51 @@ int main(int argc, char *argv[])
     i = 0;
     int * id = &i;
     char * nwk_ptr = code;
+    if (verbose) printf("reading branch lengths and computing distances\n");
     read_tree(&nwk_ptr, curr_dist, weights, names, id, n_leaves, 1);
 
-    int j;
-    for (i = 0; i < n_leaves; i++) {
-        for (j = i+1; j < n_leaves; j++) {
-        printf("%0.6f\n", weights[i*n_leaves + j - (i+1)*(i+2)/2]);
-        }
-    }
+    hid_t dataset, dataspace, h5file, dtype;
+    hsize_t dimsf[] = { ((n_leaves*(n_leaves-1))/2) };
 
-    hid_t dataset, datatype, dataspace, h5file;
-    herr_t status;
+    if (verbose) printf("writing file to %s\n", h5_outpath);
+    h5file = H5Fcreate (h5_outpath, H5F_ACC_TRUNC, H5P_DEFAULT, H5P_DEFAULT);
 
-    h5file = H5Fcreate("test.h5", H5F_ACC_TRUNC, H5P_DEFAULT, H5P_DEFAULT);
-    const hsize_t dimsf[] = {((n_leaves*(n_leaves-1))/2)};
     dataspace = H5Screate_simple(1, dimsf, NULL);
 
-    datatype = H5Tcopy(H5T_IEEE_F64LE);
-    status = H5Tset_order(datatype, H5T_ORDER_LE);
+    if (verbose) printf("writing dataset to %s\n", h5_dset_path);
+    dataset = H5Dcreate (h5file, h5_dset_path, H5T_IEEE_F64LE, dataspace, H5P_DEFAULT,
+                H5P_DEFAULT, H5P_DEFAULT);
 
-    /* Same as above, but use the property list */
-    dataset = H5Dcreate1(h5file, "/distances", datatype, dataspace, H5P_DEFAULT);
-
-    status = H5Dwrite(dataset, H5T_NATIVE_INT, H5S_ALL, H5S_ALL, H5P_DEFAULT, weights);
+    H5Dwrite(dataset, H5T_NATIVE_DOUBLE, H5S_ALL, H5S_ALL, H5P_DEFAULT, weights);
 
     H5Dclose(dataset);
     H5Sclose(dataspace);
-    H5Tclose(datatype);
+
+    dimsf[0] = n_leaves;
+
+    dtype = H5Tcopy (H5T_C_S1);
+    H5Tset_size (dtype, H5T_VARIABLE);
+
+    /*
+     * Create dataspace.  Setting maximum size to NULL sets the maximum
+     * size to be the current size.
+     */
+    dataspace = H5Screate_simple (1, dimsf, NULL);
+
+    /*
+     * Create the dataset and write the variable-length string data to
+     * it.
+     */
+    dataset = H5Dcreate (h5file, h5_names, dtype, dataspace, H5P_DEFAULT, H5P_DEFAULT,
+                H5P_DEFAULT);
+    H5Dwrite (dataset, dtype, H5S_ALL, H5S_ALL, H5P_DEFAULT, names);
+
+
     H5Fclose(h5file);
 
     free(curr_dist);
     free(weights);
     free(code);
     free(names);
-
+    if (verbose) printf("done\n");
 }
